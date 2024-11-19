7a9e8b816cd292ada4cfd13f9b50460147ae6d60
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-11-19 16:33:12 -0600

    Added content to Header
    This contains my full name and job title. Using Gsap I added in some animations for all the content to come in. As well, as a background that slowly fades from one corner to the other.

--- port/src/components/Header.jsx
+++ port/src/components/Header.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import SplitType from "split-type";
 
 export default function Header() {
+
+
+  useEffect(() => {
+    const tl = gsap.timeline({ repeat: -1, yoyo: true });
+    const Texttl = gsap.timeline({});
+
+    tl.to("#header", {
+      backgroundImage: "linear-gradient(to right bottom, #FFFFFF , #EF9880",
+      duration: 10,
+    });
+
+    Texttl.to("#first .char", {
+      x: 0,
+      stagger: 0.04,
+      duration: 0.1,
+      delay: 0.5,
+      opacity: 1,
+      rotationX: 0,
+    }).to("#second .char", {
+      y: 0,
+      stagger: 0.04,
+      duration: 0.1,
+      opacity: 1,
+      onComplete: () => {
+        setTimeout(() => {
+          document.getElementById("second").style.clipPath = "none";
+        }, 500);
+      },
+    });
+  }, []);
+
   return (
-    <div>
-      
+    <div
+      id="header"
+      className="w-full h-screen flex justify-center items-center overflow-x-hidden"
+      style={{
+        backgroundImage: "linear-gradient(to right top, #EF9880, #FFFFFF)",
+      }}
+    >
+      <div className="w-9/12 h-full flex flex-col justify-center items-center text-center">
+        <div className="text-9xl w-full">
+          <div className="w-full font-black italic cursor-pointer relative">
+            <h1 id="first" className="w-full h-fit font-narrow ">
+              EVAN BREIDECKER.{" "}
+              <span
+                id="second"
+                className="text-6xl"
+                style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}
+              >
+                Front-End Developer
+              </span>
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
