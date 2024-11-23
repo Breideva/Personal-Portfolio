e999a98a0bf511fa6334e03f3b09dbe22d51cfb3
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-11-23 15:31:33 -0600

    Added Projects to Home page

--- port/src/pages/Home.jsx
+++ port/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import Projects from "../components/Projects";
+
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative">
       <Header />
+      <Projects />
     </div>
   );
 }
