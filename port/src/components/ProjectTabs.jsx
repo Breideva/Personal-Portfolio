f765933b0829954919f74667f9a01a71411face6
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-02 14:36:18 -0600

    Revised ProjectTabs and created ProjectData
    Decided it would be easier to use a json file for content and map through it so created ProjectData which holds all six projects. ProjectTabs works pretty similar except for mapping through the data and making it so I don't have to set the states I just pass them through to DisplayProject.

--- port/src/components/ProjectTabs.jsx
+++ port/src/components/ProjectTabs.jsx
@@ -1,59 +1,86 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { FaGithub, FaGlobe } from "react-icons/fa";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import data from "../data/ProjectData.json";
+import MediumWide from "../assets/GrassBeach.avif";
 
-export default function ProjectTabs({
-  title = "",
-  type = "",
-  desc,
-  lang = [],
-  height,
-  image,
-}) {
-  const [click, setClick] = useState(false);
-  const langs = Array.isArray(lang) ? lang : [];
+export default function ProjectTabs({}) {
 
-  const [selectedTitle, setSelectedTitle] = useState();
-  const [selectedLang, setSelectedLang] = useState();
-  const [selectedType, setSelectedType] = useState();
-  const [selectedDesc, setSelectedDesc] = useState();
+
+  // const langs = Array.isArray(lang) ? lang : [];
 
   // useEffect(() => {
   //   console.log("Selected Title:", type);
   // }, [selectedType]);
 
-  const clicked = () => {
-    setSelectedTitle(title);
-    setSelectedType(type);
-    setSelectedLang(lang);
-    setSelectedDesc(desc);
-  };
+  // const clicked = () => {
+  //   setSelectedTitle(item.title);
+  //   setSelectedType(type);
+  //   setSelectedLang(lang);
+  //   setSelectedDesc(desc);
+  // };
 
   return (
-    <div className="w-fit h-fit hover:scale-105 transition-all duration-300">
-      <div className="h-fit">
-        <img src={image} className="h-fit w-fit bg-slate-950 " />
-      </div>
-      <div className="h-fit">
-        <div className="flex justify-between items-center">
-          <Link
-            to="/project"
-            state={{ selectedTitle: title, selectedType: type, selectedDesc: desc, selectedLang: lang }}
-            onClick={clicked}
-            className="text-4xl font-bold italic font-narrow"
+    <div className="w-full">
+      <div className="flex-wrap w-fit">
+        {data.projects.map((item, index) => (
+          <div
+            className="flex flex-col p-12 rounded-md mb-8 hover:scale-105 transition-all duration-300 bg-bgBox"
+            key={index}
           >
-            {title}
-          </Link>{" "}
-          <h3 className="text-2xl font-narrow font-semibold">{type}</h3>
-        </div>
-        <p className="text-base font-light">{desc}</p>
-        <div className="text-lg font-medium flex gap-1">
-          {langs.map((items, index) => (
-            <p key={index}>
-              {items}
-              {index < langs.length - 1 ? "," : ""}
-            </p>
-          ))}
-        </div>
+            <div className="flex flex-col lg:flex-row items-center justify-between text-center lg:text-start">
+              <div>
+                <div className="flex items-center justify-center lg:justify-start gap-2">
+                  <Link
+                    to="/project"
+                    state={{
+                      title: item.title,
+                      desc: item.desc,
+                      lang: item.lang,
+                      type: item.type,
+                    }}
+                    className="text-textLoud text-4xl md:text-7xl font-semibold hover:text-textLoudHover transition-all duration-300"
+                  >
+                    {item.title}
+                  </Link>
+                  {item.icons == "github" ? (
+                    <p className=" text-xl lg:text-3xl">
+                      <a href="https://github.com/Breideva">
+                        <FaGithub />
+                      </a>
+                    </p>
+                  ) : (
+                    <div className="flex gap-2 text-xl md:text-3xl">
+                      <a href="https://github.com/Breideva">
+                        <FaGithub />
+                      </a>
+                      <a href="https://msmboatcare.com/">
+                        <FaGlobe />
+                      </a>
+                    </div>
+                  )}
+                </div>
+                <p className="font-thin text-xs md:text-base lg:text-lg lg:w-1/2">
+                  {item.desc}
+                </p>
+              </div>
+              <div>
+                <h3 className=" text-xl md:text-5xl font-medium">
+                  {item.type}
+                </h3>
+                <h3 className="flex gap-1 text-base xl:text-2xl font-normal">
+                  {item.lang.map((i, index) => (
+                    <span>
+                      {i}
+                      {item.lang.length - 1 > index ? "," : ""}
+                    </span>
+                  ))}
+                </h3>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
