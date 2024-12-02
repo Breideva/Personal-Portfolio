1b62d37a7fffcc6ca7ec1b1b0364ebd0439dd15b
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-02 14:38:35 -0600

    Revised DisplayProject
    With using the data base made it easier to pass in all the information being used. Also added function to scroll to top of page every time a user enter to ensure starting at the correct point.

--- port/src/pages/DisplayProject.jsx
+++ port/src/pages/DisplayProject.jsx
@@ -1,34 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { HiArrowSmLeft } from "react-icons/hi";
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 import { Link, useLocation } from "react-router-dom";
 
 export default function DisplayProject() {
   const location = useLocation();
+  const { title, desc, lang, type } = location.state;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   useEffect(() => {
     console.log(location);
   });
 
-  const { selectedTitle } = location.state || {};
-  const { selectedType } = location.state || {};
-  const { selectedLang } = location.state || {};
-  const { selectedDesc } = location.state || {};
-
   return (
-    <div className="w-full h-[250vh] absolute z-30 flex justify-center bg-slate-400 bg-opacity-85 ">
+    <div className="w-full h-[250vh] absolute z-30 flex justify-center bg-bg">
       <div className="w-7/12 h-full flex flex-col py-8 font-narrow gap-8">
-        <div className="flex justify-between gap-8">
-          <Link to="/" className="text-2xl w-1/4">
-            <h3>Back</h3>
-            <HiArrowSmLeft className="" />
+        <div className="flex justify-between items-center gap-8">
+          <Link to="/#projects" className="text-2xl w-1/4">
+            <MdKeyboardDoubleArrowLeft className="text-4xl" />
           </Link>
           <div className="flex flex-col w-1/2 justify-center text-center">
-            <h1 className="font-black italic text-8xl">{selectedTitle}</h1>
-            <p>{selectedDesc}</p>
+            <h1 id="top" className="font-black italic text-textLoud text-8xl">
+              {title}
+            </h1>
+            <p className="text-lg">{desc}</p>
           </div>
           <div className="flex flex-col gap-2 w-1/4 items-end justify-center text-center">
-            {selectedLang.map((item, index) => (
-              <div className="text-3xl border-2 border-black w-1/2" key={index}>
+            {lang.map((item, index) => (
+              <div className="text-2xl border-2 border-text w-1/2" key={index}>
                 {item}
               </div>
             ))}
