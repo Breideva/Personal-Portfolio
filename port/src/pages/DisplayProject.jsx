0d398e8eb74129f707e23aa05e726fef004d0ad8
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-24 15:02:17 -0600

    Added animation and styles to DisplayProject(NOT IN USE)
    Added animations and cleaned up some sizing styles for this page. Although no longer being used.

--- port/src/pages/DisplayProject.jsx
+++ port/src/pages/DisplayProject.jsx
@@ -1,13 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 import { Link, useLocation } from "react-router-dom";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function DisplayProject() {
   const location = useLocation();
-  const { title, desc, lang, type } = location.state;
+  const { title, desc, lang, type, imgs } = location.state;
 
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    const tl = gsap.timeline({});
+
+    gsap.to(".title", {
+      opacity: 1,
+      x: 0,
+      duration: 0.2,
+    });
+
+    gsap.to("#items > div", {
+      opacity: 1,
+      y: 0,
+      duration: 0.8,
+      stagger: 0.15,
+    });
+    gsap.to("#img0", {});
+    imgs.forEach((_, index) => {
+      tl.to("#img0", {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+      }).to(`#img${index}`, {
+        scrollTrigger: {
+          trigger: index == 0 ? ".title" : `#img${index - 1}`,
+          start: "center 20%",
+          end: "top 20%",
+          scrub: 1,
+          markers: true,
+        },
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+      });
+    });
   }, []);
 
   useEffect(() => {
@@ -15,30 +53,48 @@ export default function DisplayProject() {
   });
 
   return (
-    <div className="w-full h-[250vh] absolute z-30 flex justify-center bg-bg">
+    <div className="w-full absolute z-30 flex justify-center bg-bg">
       <div className="w-7/12 h-full flex flex-col py-8 font-narrow gap-8">
-        <div className="flex justify-between items-center gap-8">
-          <Link to="/#projects" className="text-2xl w-1/4">
-            <MdKeyboardDoubleArrowLeft className="text-4xl" />
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-4 lg:gap-8">
+          <Link to="/#projects" className="text-2xl lg:w-1/4 title">
+            <MdKeyboardDoubleArrowLeft className="text-3xl sm:text-5xl lg:text-7xl" />
           </Link>
-          <div className="flex flex-col w-1/2 justify-center text-center">
-            <h1 id="top" className="font-black italic text-textLoud text-8xl">
+          <div
+            // id="title"
+            className="flex flex-col lg:w-1/2 justify-center text-center title"
+          >
+            <h1 className="font-black italic text-textLoud text-6xl sm:text-7xl md:text-8xl">
               {title}
             </h1>
-            <p className="text-lg">{desc}</p>
+            <p className="text-base md:text-lg xl:text-xl">{desc}</p>
           </div>
-          <div className="flex flex-col gap-2 w-1/4 items-end justify-center text-center">
+          <div
+            id="items"
+            className="flex flex-row lg:flex-col flex-wrap gap-2 lg:w-1/4 md:items-end justify-center text-center"
+          >
             {lang.map((item, index) => (
-              <div className="text-2xl border-2 border-text w-1/2" key={index}>
+              <div
+                className="text-lg md:text-xl p-1 lg:p-0 xl:text-2xl border-2 border-text lg:w-1/2"
+                id={`tech${index}`}
+                key={index}
+              >
                 {item}
               </div>
             ))}
           </div>
         </div>
-        <div className="flex flex-col gap-8">
-          <div className="bg-zinc-800 w-full h-[65vh]"></div>
-          <div className="bg-zinc-800 w-full h-[65vh]"></div>
-          <div className="bg-zinc-800 w-full h-[65vh]"></div>
+        <div className="flex flex-col">
+          {imgs.map((image, index) => (
+            <div className="w-full text-center" key={index}>
+              <img
+                id={`img${index}`}
+                loading="lazy"
+                className="w-full"
+                src={image}
+                alt=""
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
